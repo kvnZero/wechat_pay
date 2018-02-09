a2fce602570675ff50ac651ec369c14b8ce7e341
sohyunQVQ <abigeater@163.com>
2018-02-09 19:08:36 +0800

    push file

--- wechat_pay.py
+++ wechat_pay.py
@@ -3,41 +3,28 @@ import re
 import time
 import datetime
 
-class wechat_pay():
+@itchat.msg_register(itchat.content.SHARING)
+def text_reply(msg):
+	returnmsg = {}
+	returnmsg['content'] = msg.Content
 
-	def __init__(self):
-		self.printlog = False
-		itchat.auto_login()
+	try:
+		returnmsg['fromuser'] = msg.fromUserName
+		returnmsg['note'] = re.findall(re.compile(r'付款方备注：(.*?)\n'), returnmsg['content'])[0]
+		returnmsg['money'] = re.findall(re.compile(r'微信支付收款(.*?)元'), returnmsg['content'])[0]
+		returnmsg['createtime'] = datetime.datetime.utcnow()
+	except IndexError:
+		pass
 
-	def run(self):
-		itchat.run()
-
-	@itchat.msg_register(itchat.content.SHARING)
-	def text_reply(msg):
-		returnmsg = {}
-		returnmsg['content'] = msg.Content
-
-		try:
-			returnmsg['fromuser'] = msg.fromUserName
-			returnmsg['note'] = re.findall(re.compile(r'付款方备注：(.*?)\n'), content)[0]
-			returnmsg['money'] = re.findall(re.compile(r'微信支付收款(.*?)元'), content)[0]
-			returnmsg['createtime'] = datetime.datetime.utcnow()
-		except IndexError:
-			pass
-
-		if returnmsg.note and returnmsg.money:
-			if printlog:
-				return printlog(returnmsg)
-			else:
-				return returnmsg
+	if returnmsg['note'] and returnmsg['money']:
+		if printlog:
+			logstr = "From:%s, Money:%s, Note:%s, Time:%s" % (returnmsg.fromuser, returnmsg.money, returnmsg.note, returnmsg.createtime)
+			print(logstr)
 		else:
-			return null
-
-	def printlog(self, returnmsg):
-		logstr = "From:%s, Money:%s, Note:%s, Time:%s" % (returnmsg.fromuser, returnmsg.money, returnmsg.note, returnmsg.createtime)
-		print(logstr)
-
+			return returnmsg
+	else:
+		return null
 
 if __name__ == '__main__':
-	wechat_pay = wechat_pay()
-	wechat_pay.run()
+	itchat.auto_login()
+	itchat.run()
