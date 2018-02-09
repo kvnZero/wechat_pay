90c9f98c155c2a26d0300f73e4e1d2e66ce12bbd
sohyunQVQ <abigeater@163.com>
2018-02-09 19:11:03 +0800

    push file

--- wechat_pay.py
+++ wechat_pay.py
@@ -2,6 +2,8 @@ import itchat
 import re
 import time
 import datetime
+PRINT_LOG = True
+
 
 @itchat.msg_register(itchat.content.SHARING)
 def text_reply(msg):
@@ -10,20 +12,18 @@ def text_reply(msg):
 
 	try:
 		returnmsg['fromuser'] = msg.fromUserName
-		returnmsg['note'] = re.findall(re.compile(r'付款方备注：(.*?)\n'), returnmsg['content'])[0]
 		returnmsg['money'] = re.findall(re.compile(r'微信支付收款(.*?)元'), returnmsg['content'])[0]
 		returnmsg['createtime'] = datetime.datetime.utcnow()
+		returnmsg['note'] = re.findall(re.compile(r'付款方备注：(.*?)\n'), returnmsg['content'])[0]
 	except IndexError:
-		pass
+		returnmsg['note'] = ""
 
 	if returnmsg['note'] and returnmsg['money']:
-		if printlog:
+		if PRINT_LOG:
 			logstr = "From:%s, Money:%s, Note:%s, Time:%s" % (returnmsg.fromuser, returnmsg.money, returnmsg.note, returnmsg.createtime)
 			print(logstr)
 		else:
 			return returnmsg
-	else:
-		return null
 
 if __name__ == '__main__':
 	itchat.auto_login()
